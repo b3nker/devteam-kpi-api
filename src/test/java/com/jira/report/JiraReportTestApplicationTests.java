f068db13c3c67ae8b3b719be5a1d7aa8f0e63121
David Moulin <david.moulin@neo9.fr>
2020-09-03 14:47:34 +0200

    fix: test without db

--- src/test/java/com/jira/report/JiraReportTestApplicationTests.java
+++ src/test/java/com/jira/report/JiraReportTestApplicationTests.java
@@ -1,13 +1,15 @@
 package com.jira.report;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@DataMongoTest
+@ExtendWith(SpringExtension.class)
 class JiraReportTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
