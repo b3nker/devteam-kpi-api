a61114f138762f2f2271ce5fc46d1ff510601d8c
David Moulin <david.moulin@neo9.fr>
2020-09-10 17:39:23 +0200

    app: api documentation

--- src/main/java/com/jira/report/controller/SprintController.java
+++ src/main/java/com/jira/report/controller/SprintController.java
@@ -3,6 +3,8 @@ package com.jira.report.controller;
 import com.jira.report.model.entity.SprintEntity;
 import com.jira.report.service.SprintService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,22 +15,29 @@ import java.util.Collection;
 @RestController
 @Api(value = "Sprint endpoints")
 public class SprintController {
+
     private final SprintService sprintService;
 
     public SprintController(SprintService sprintService) {
         this.sprintService = sprintService;
     }
 
+    @ApiResponse(code = 200, message = "Sprints for all team retrieved")
+    @ApiOperation(value = "Get all sprints of all teams")
     @GetMapping(value = "/sprint")
     public Collection<SprintEntity> getSprintTeam() {
         return sprintService.getSprint();
     }
 
+    @ApiResponse(code = 200, message = "Sprints for a team retrieved")
+    @ApiOperation(value = "Get all sprints for a team")
     @GetMapping(value = "/sprint/{teamName}")
     public Collection<SprintEntity> getAllSprintForATeam(@PathVariable("teamName") String teamName) {
         return sprintService.getSprintsTeam(teamName);
     }
 
+    @ApiResponse(code = 200, message = "Last sprint retrieved")
+    @ApiOperation(value = "Get the last sprint for a team")
     @GetMapping(value = "/sprint/{teamName}/last")
     public SprintEntity getLastSprintOfTeam(@PathVariable("teamName") String teamName) {
         return sprintService.getSprintTeam(teamName);
--- src/main/java/com/jira/report/service/SprintService.java
+++ src/main/java/com/jira/report/service/SprintService.java
@@ -5,16 +5,12 @@ import com.jira.report.dao.api.API;
 import com.jira.report.model.entity.SprintEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class SprintService {
@@ -30,7 +26,7 @@ public class SprintService {
     }
 
     @Transactional
-    public void loadSprints(){
+    public void loadSprints() {
         LOGGER.info("Starting to construct Sprints objects");
         Collection<SprintEntity> sprints = api.callJiraSprintAPI().values();
         sprintDao.saveAll(sprints);
@@ -49,15 +45,11 @@ public class SprintService {
 
     @Transactional(readOnly = true)
     public SprintEntity getSprintTeam(String teamName) {
-        Set<SprintEntity> sprints  = sprintDao.findByTeamName(teamName)
+        return sprintDao.findByTeamName(teamName)
                 .stream()
                 .filter(sprintEntity -> sprintEntity.getEndDate().isAfter(LocalDateTime.now()))
-                .collect(Collectors.toSet());
-        if (sprints.size() == 1){
-            return sprints.stream().findFirst().get();
-        }else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Last sprint for team  " + teamName + " not found");
-        }
+                .findFirst()
+                .get();
     }
 }
 
